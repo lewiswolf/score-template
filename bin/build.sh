3d9c70bb775833d4401cef802a2436cd556ffb1f
lewiswolf <lewiswolstanholme@gmail.com>
2022-12-01 12:26:04 +0000

    fixed error with unused options

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
+# build all .tex files
 pdflatex --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error index.tex
-rm index.aux
-rm index.log
-rm index.synctex.gz
+
+# delete all junk files
