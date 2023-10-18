af6a550ebc9c12d668357ad5a12a61c66d17d0f2
lewiswolf <lewiswolstanholme@gmail.com>
2023-10-18 14:08:49 +0100

    fixed build script not recognising filenames with spaces

--- bin/build.sh
+++ bin/build.sh
@@ -7,8 +7,8 @@ for f in *
 do
 	case $f in
 	*.tex)
-		xelatex --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error $f
-		xelatex --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error $f
+		xelatex --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error "$f"
+		xelatex --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error "$f"
 		;;
 	*)
 		continue
@@ -21,7 +21,7 @@ for f in *
 do
 	case $f in
 	*.aux|*.log|*.out|*synctex.gz)
-		rm $f
+		rm "$f"
 		;;
 	*)
 		continue
